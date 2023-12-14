aeb376ddc21d7b37827be5f77971cd0addc074fe
Andres Parra <andresparravfto@gmail.com>
2023-12-14 01:09:56 -0500

    Commit crecion backend pcgamingcomco

---
+++ pcgamingcomco/src/main/java/com/pcgamingcomco/PcgamingcomcoApplication.java
@@ -0,0 +1,13 @@
+package com.pcgamingcomco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcgamingcomcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcgamingcomcoApplication.class, args);
+	}
+
+}
---
+++ pcgamingcomco/src/test/java/com/pcgamingcomco/PcgamingcomcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcgamingcomco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcgamingcomcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
